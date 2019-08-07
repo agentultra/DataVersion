06b0a7f1e9409111b954c4784ac7600b3f8ccf28
James King <james@agentultra.com>
2019-08-07 15:10:08 -0400

    Modified test/Spec.hs
    Added quickcheck property for Transform

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,10 @@
+import Data.String
+import GHC.Generics
+import GHC.TypeLits
+import Test.QuickCheck
+
+import Data.Migration
+
 data family Foo (a :: Nat)
 
 newtype MyString = MyString { unMyString :: String }
@@ -9,6 +16,12 @@ data instance Foo 0
     , _fooName :: String
     }
   deriving (Generic, Show, Eq)
+
+instance Arbitrary (Foo 0) where
+  arbitrary = do
+    fooId <- arbitrary
+    fooName <- arbitrary
+    pure $ FooV0 fooId fooName
 
 data instance Foo 1
   = FooV1
@@ -22,5 +35,8 @@ instance Transform Foo 0 where
   up   v = genericUp   v (const "esquire") (const MyString)
   down v = genericDown v (const unMyString)
 
+prop_inverse_transform :: Foo 0 -> Bool
+prop_inverse_transform v = (down $ up v) == v
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = quickCheck prop_inverse_transform
